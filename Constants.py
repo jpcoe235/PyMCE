affe5ed619cf0759496839d9e443a44f756de2f9
AMC-dyn <andresmor89@gmail.com>
2021-04-04 03:38:04 +0200

    Initial Commit

---
+++ Constants.py
@@ -0,0 +1,12 @@
+class physconst():
+    def __init__(self):
+        self.pi = 3.141592653589793
+        self.pi2 = 2.0*self.pi
+        self.pih = 0.5*self.pi
+        self.kb = 1.3806488e-23
+        self.bohr = 5.2917721092e-1
+        self.au2ev= 2.721138505e1
+        self.hbar = 1.054571726e-34
+        self.amukg = 1.660538921e-27
+        self.mekg=9.10938291e-31
+        self.amu=self.amukg/self.mekg
---
+++ main.py
@@ -0,0 +1,18 @@
+import input
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+init=input.initgeom()
+print(init.rkinit)
+dynin=input.initdyn()
+print(dynin.gamma_mass)
+
+
